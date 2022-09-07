8bee6652a72a0e70c1e06a16d87b90b8966b9e98
Maros3 <109620137+Maros3@users.noreply.github.com>
2022-09-07 15:01:49 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+
+function App() {
+
+  // Definir la categoria y noicias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=4aeff34d78804ebd9b004dab7173f77c`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
+
+  return (
+  <Fragment>
+    <Header 
+      titulo='Buscador de noticias'
+    />
+
+    <div className='container white'>
+      <Formulario 
+        guardarCategoria={guardarCategoria}
+      />
+
+      <ListadoNoticias 
+        noticias={noticias}
+      />
+    </div>
+  </Fragment>
+  );
+}
+
+export default App;
